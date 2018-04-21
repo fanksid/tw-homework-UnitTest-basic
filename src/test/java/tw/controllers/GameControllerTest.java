a0eab4f7c9f111076bba816c594e6a5a993c02d8
superxc <superxcgm@gmail.com>
2018-04-21 10:52:49 +0800

    update GameControllerTest

--- src/test/java/tw/controllers/GameControllerTest.java
+++ src/test/java/tw/controllers/GameControllerTest.java
@@ -1,46 +1,47 @@
 package tw.controllers;
 
-import com.google.inject.Injector;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import tw.GuessNumberModule;
+import tw.commands.InputCommand;
+import tw.core.Game;
+import tw.views.GameView;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
-import static com.google.inject.Guice.createInjector;
+import static org.mockito.Mockito.*;
 
 /**
  * 在GameControllerTest文件中完成GameController中对应的单元测试
  */
 public class GameControllerTest {
-    private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private GameController gameController;
+    private Game mockGame;
+    private GameView mockGameView;
+    private InputCommand mockInputCommand;
 
     @Before
     public void setUp() throws Exception {
-        Injector injector = createInjector(new GuessNumberModule());
-        gameController = injector.getInstance(GameController.class);
-        System.setOut(new PrintStream(outContent));
+        mockGame = mock(Game.class);
+        mockGameView = mock(GameView.class);
+        mockInputCommand = mock(InputCommand.class);
+        gameController = new GameController(mockGame, mockGameView);
     }
 
     @Test
     public void should_print_begin_message() throws Exception{
         gameController.beginGame();
-        String result = "------Guess Number Game, You have 6 chances to guess!  ------\n";
-        Assert.assertEquals(systemOut(), result);
+        verify(mockGameView, times(1)).showBegin();
     }
 
-    private String systemOut() {
-        return outContent.toString();
+    @Test
+    public void should_continue_when_game_checkContinue_return_true() throws Exception{
+        when(mockGame.checkCoutinue()).thenReturn(true).thenReturn(false);
+        gameController.play(mockInputCommand);
+        verify(mockGameView, times(1)).showGuessHistory(any());
     }
 
-//     TODO: 考虑对play的测试
-//    1. 猜6次都没猜对
-//    2. 猜1次就对
-//    3. 第6次猜对
-//    4. 第5次猜对
-//    问题在于无法手动设置正确答案
-
+    @Test
+    public void should_showGameStatus_when_checkContinue_return_false() throws Exception{
+        when(mockGame.checkCoutinue()).thenReturn(false);
+        gameController.play(mockInputCommand);
+        verify(mockGameView, times(1)).showGameStatus(any());
+    }
 }
